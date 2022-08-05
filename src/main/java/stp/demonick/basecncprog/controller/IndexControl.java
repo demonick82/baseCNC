77cb21d593d9bccda9ebe3afda312f6ab8dc827f
demonick82 <demonick82@gmail.com>
2022-08-05 14:49:22 +0300

    edit front

--- src/main/java/stp/demonick/basecncprog/controller/IndexControl.java
+++ src/main/java/stp/demonick/basecncprog/controller/IndexControl.java
@@ -1,5 +1,7 @@
 package stp.demonick.basecncprog.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +20,9 @@ public class IndexControl {
 
     @GetMapping({"/", "/index"})
     public String index(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("details", detailService.findAllDetails());
+        model.addAttribute("username", auth.getName());
         return "index";
     }
 
